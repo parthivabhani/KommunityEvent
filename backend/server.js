e6120d158bcffef92167738c93a7253a2911680e
parthivabhani <142013527+parthivabhani@users.noreply.github.com>
2025-11-09 17:53:23 +0530

    Add a root route to confirm backend is running

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,11 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
